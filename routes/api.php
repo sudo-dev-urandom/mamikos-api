3753ca6912a35252cc94b9f7cda113f185d17465
Muhammad Safreza <lord.ezza@gmail.com>
2021-12-04 11:34:54 +0700

    add auth crud

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +14,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/user',
+    function (Request $request) {
+        return $request->user();
+    }
+);
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
 });
+
